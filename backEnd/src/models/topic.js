86f7e42c30378c56e0e20cd18e4650c0e993b02e
Jonathan Cárdenas <jonym97156@gmail.com>
2019-05-12 22:19:55 -0500

    Some changes in Topic schema, not relevant

--- backEnd/src/models/topic.js
+++ backEnd/src/models/topic.js
@@ -1,9 +1,25 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 const topicSchema = new mongoose.Schema({
-    name: String,
-    materia: String
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    subject: {
+        type: String,
+        required: true
+    }
 })
+
+//Relacion entre schemas
+topicSchema.virtual('hasSubject', {
+    ref: 'Subject',
+    localfield: 'subject',
+    foreignField: 'name'
+})
+
 
 const Topic = mongoose.model('Topic',topicSchema)
 module.exports = Topic
