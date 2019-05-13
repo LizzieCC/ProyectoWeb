719559d99dffd3fcfd2645696f39a72138e565d9
Jonathan Cárdenas <jonym97156@gmail.com>
2019-05-13 16:14:06 -0500

    Ready for heroku

---
+++ src/config.js
@@ -0,0 +1,3 @@
+module.exports.connectionURL = 'mongodb+srv://admin:HelloMoto@cluster0-fnftg.mongodb.net/preparatemas?retryWrites=true'
+
+module.exports.secret = 'SuperSecretKeyOfJonathanAndLizzie'
